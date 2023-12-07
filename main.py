d2081508204a4eff62b7c9c9d8a460a3b3f70cae
tupilogo <tupi.logogestao@gmail.com>
2023-12-07 17:55:26 -0300

    Buscar pelo nome do arquivo

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ import tkinter as tk
 from tkinter import scrolledtext, messagebox
 import os
 from tkinter.filedialog import askdirectory
+import re
 
 
 def buscar_arquivo():
     caminho = input_caminho.get()
-    extensao = input_extensao.get()
+    extensao = input_extencao.get()
+    nome_arquivo = input_nome_arquivo.get()
+    
 
     # Adiciona a mensagem ao widget Text
     output_text.insert(tk.END, "Tenha pasciência, estou entrando no limbo...\n\n")
     output_text.update_idletasks()
     
     # Buscar arquivos .pst no diretório e subdiretórios
-    arquivos_encontrados = __buscador_de_arquivos(nome_arquivo="tupi", diretorio=caminho, extencao=extensao)
+    arquivos_encontrados = __buscador_de_arquivos(nome_arquivo=nome_arquivo, diretorio=caminho, extencao=extensao)
 
     # Mostrar os arquivos encontrados
     if arquivos_encontrados:
@@ -44,11 +47,13 @@ def limpar_tela():
     output_text.configure(state='disabled')
 
 
-def __buscador_de_arquivos(nome_arquivo:str, diretorio, extencao):
+def __buscador_de_arquivos(nome_arquivo:str, diretorio, extencao:str):
     arquivos_pst = []
-    for pasta_raiz, sub_pastas, arquivos in os.walk(diretorio):
+    extencao = extencao.lower()
+
+    for pasta_raiz, _, arquivos in os.walk(diretorio):
         for arquivo in arquivos:
-            if arquivo.endswith(extencao) and nome_arquivo in arquivo:
+            if arquivo.lower().endswith(extencao) and nome_arquivo in arquivo:
                 arquivos_pst.append(os.path.join(pasta_raiz, arquivo))
     return arquivos_pst
 
@@ -78,9 +83,11 @@ def selecionar_caminho_pasta(elemento: tk.Entry) -> str:
 root = tk.Tk()
 frame_buscar_arquivo = tk.Frame(root)
 frame_extencao_arquivo = tk.Frame(root)
+frame_nome_do_arquivo = tk.Frame(root)
 frame_envio_requisicao = tk.Frame(root)
 
 frame_buscar_arquivo.pack(side=tk.TOP, padx=10, pady=10)
+frame_nome_do_arquivo.pack(side=tk.TOP, padx=10, pady=10)
 frame_extencao_arquivo.pack(side=tk.TOP, padx=10, pady=10)
 frame_envio_requisicao.pack(side=tk.BOTTOM, padx=10, pady=10)
 
@@ -96,13 +103,20 @@ input_caminho.pack(side=tk.LEFT)
 botao_buscar_pasta = tk.Button(frame_buscar_arquivo, text="Buscar Pasta...", command=lambda: selecionar_caminho_pasta(elemento=input_caminho))
 botao_buscar_pasta.pack(side=tk.LEFT, padx=5)
 
+# Rótulo do input do nome do nome do arquivo
+label_nome_arquivo = tk.Label(frame_nome_do_arquivo, text="Nome do arquivo:")
+label_nome_arquivo.pack(side=tk.LEFT)
+# Entrada para o nome do arquivo
+input_nome_arquivo = tk.Entry(frame_nome_do_arquivo, width=20)
+input_nome_arquivo.pack(side=tk.LEFT)
 
 # Rótulo do input da extenção do arquivo
-label_extensao = tk.Label(frame_extencao_arquivo, text="Extensão do arquivo:")
-label_extensao.pack(side=tk.LEFT)
+label_extencao = tk.Label(frame_extencao_arquivo, text="Extensão do arquivo:")
+label_extencao.pack(side=tk.LEFT)
 # Entrada para a extensão do arquivo
-input_extensao = tk.Entry(frame_extencao_arquivo, width=5)
-input_extensao.pack(side=tk.LEFT)
+input_extencao = tk.Entry(frame_extencao_arquivo, width=5)
+input_extencao.pack(side=tk.LEFT)
+
 
 # Mostrar o status do sistema
 output_text = scrolledtext.ScrolledText(root, wrap=tk.WORD)
